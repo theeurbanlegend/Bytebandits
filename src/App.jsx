2d100564978656194a0ccea1148f3b45bf1fccd8
jonniek01 <jontejonhn@gmail.com>
2023-06-25 17:24:33 +0300

    importing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import React from 'react'
+import Navigation from './Navigation'
 
 function App() {
  
   return(
     <div>
+      <Navigation />
       <h1>Byte Bandits</h1>
+      
+
     </div>
   )
 }
