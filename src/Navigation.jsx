f53da70ebe3692e78a971cf7fd660ef9a0614988
jonniek01 <jontejonhn@gmail.com>
2023-06-25 17:22:25 +0300

    navigatio bar creation

---
+++ src/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div>
+        <h2>Byte Bandits</h2>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contacts</li>
+
+            </ul>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navigation
