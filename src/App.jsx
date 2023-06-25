087955c0e9ee8cb13ab6431ad82b5b2a4631a37a
theeurbanlegend <mbuto152@gmail.com>
2023-06-25 17:53:00 +0300

    Added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import React from 'react'
 import Navigation from './Navigation'
-
+import Projects from './Projects'
 function App() {
  
   return(
     <div>
       <Navigation />
       <h1>Byte Bandits</h1>
-      
+      <Projects/>
 
     </div>
   )
